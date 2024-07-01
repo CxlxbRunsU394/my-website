0bef7b4cfeafcfe75e62616c2d5029ebb5c33118
! vibez <153403928+CxlxbRunsU394@users.noreply.github.com>
2024-07-01 13:04:13 -0700

    Add files via upload

--- bio.js
+++ bio.js
@@ -5,8 +5,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
     overlay.addEventListener('click', () => {
         audio.play();
         overlay.classList.add('hide');
-        setTimeout(() => {
-            overlay.style.display = 'none';
-        }, 500); 
+
+        const dark = document.createElement('div');
+        dark.classList.add('dark-overlay');
+        document.body.appendChild(dark);
+
+        const pfpic = document.createElement('img');
+        pfpic.src = 'pfp.png';
+        pfpic.classList.add('profile-pic');
+        document.body.appendChild(pfpic);
     });
 });
